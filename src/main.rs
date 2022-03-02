2f61d7b5fe30b8d724256aaed258a82ac61a4da2
Jacob Elder <jacob@blissfully.com>
2022-03-02 12:40:26 -0500

    Add progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use indicatif::*;
 use postgres::{Client, NoTls};
 use std::collections::HashMap;
 use std::error::Error;
@@ -7,15 +8,24 @@ static DATABASE_URL: &str = "postgres://localhost:15432/postgres";
 
 static SCHEMA: &str = "saasdash";
 
-static REFLECT_QUERY: &str = r#"
+static REFLECT_COLUMNS_QUERY: &str = r#"
     select
         table_name,
         column_name,
         case is_nullable when 'YES' then True else False end as is_nullable,
-        ordinal_position
+        ordinal_position,
+        pg_relation_size(quote_ident(table_name)) as table_size
     from information_schema.columns
     where table_schema = 'saasdash'
     order by table_name, ordinal_position;
+"#;
+
+static REFLECT_TABLES_QUERY: &str = r#"
+    select
+        table_name,
+        pg_relation_size(quote_ident(table_name)) as table_size
+    from information_schema.tables
+    where table_schema = 'saasdash'
 "#;
 
 static PARTITION_COLUMN: &str = "OrganizationId";
@@ -32,20 +42,25 @@ fn try_main() -> Result<(), Box<dyn Error>> {
     // The weird `COPY TO` behavior requires some acrobatics that we avoid by
     // just having a separate connection for it.
     let mut copy_client = Client::connect(DATABASE_URL, NoTls)?;
-    for table in get_tables(client)?.iter() {
+
+    let tables = get_tables(client)?;
+    let pb = ProgressBar::new(tables.iter().fold(0, |acc, table| acc + table.size) as u64);
+
+    for table in tables.iter() {
         let copy_statement = format!(
             r#"COPY ({}) TO stdout;"#,
             table.copy_out_query(PARTITION_VALUE)
         );
+        pb.println(format!("Dumping {} ({} bytes)", &table.name, table.size));
         let mut reader = copy_client.copy_out(&copy_statement)?;
         let mut buf = vec![];
         reader.read_to_end(&mut buf)?;
         println!("BEGIN;");
         println!(r#"TRUNCATE TABLE {}."{}";"#, SCHEMA, table.name);
         println!("{};", table.copy_in_query());
-        println!("{}", std::str::from_utf8(&buf)?);
-        println!("\\.");
+        println!("{}\\.", std::str::from_utf8(&buf)?);
         println!("COMMIT;");
+        pb.inc(table.size as u64);
     }
 
     Ok(())
@@ -53,26 +68,48 @@ fn try_main() -> Result<(), Box<dyn Error>> {
 
 #[derive(Debug, Clone)]
 struct Table {
-    pub name: String,
-    // pub query: String,
-    pub columns: Vec<Column>,
+    name: String,
+    columns: Vec<Column>,
+    size: i64,
 }
 
 impl Table {
     fn copy_out_query(&self, partition_column_value: &str) -> String {
+        let query = match self.name.as_str() {
+            "googleAuthPermissions" => {
+                format!(
+                    r#"
+                        select {} from "googleAuthPermissions"
+                        join "googleAuthTokens" on "googleAuthTokenId" = "googleAuthTokens".id
+                        join "directoryUsers" on "GoogleUserId" = "directoryUsers"."externalId"
+                        where "directoryUsers"."OrganizationId" = '{}'
+                    "#,
+                    &self.column_list_qualified(),
+                    PARTITION_VALUE,
+                )
+            }
+            // "dailyExchangeRates" => {
+            //     self.default_copy_out_query(partition_column_value) + " where True = False"
+            // }
+            _ => self.default_copy_out_query(partition_column_value),
+        };
+        query
+    }
+
+    fn default_copy_out_query(&self, partition_column_value: &str) -> String {
         let mut query = format!(r#"select {} from "{}""#, &self.column_list(), &self.name);
         if let Some(org_scope) = self
             .columns
             .iter()
             .find(|column| column.name == PARTITION_COLUMN)
         {
+            let mut where_clause = format!(r#""{PARTITION_COLUMN}" = '{partition_column_value}'"#);
             if org_scope.is_nullable {
-                query = format!(
-                    r#"{query} where "{PARTITION_COLUMN}" = '{}'"#,
-                    partition_column_value
-                )
+                where_clause = format!(r#"({where_clause} or "{PARTITION_COLUMN}" is null)"#)
             }
+            query = format!("{query} where {where_clause}");
         }
+        query = format!("{query} limit 10");
         query
     }
     fn copy_in_query(&self) -> String {
@@ -86,6 +123,13 @@ impl Table {
         self.columns
             .iter()
             .map(|column| column.quoted_name())
+            .collect::<Vec<String>>()
+            .join(", ")
+    }
+    fn column_list_qualified(&self) -> String {
+        self.columns
+            .iter()
+            .map(|column| format!(r#""{}".{}"#, self.name, column.quoted_name()))
             .collect::<Vec<String>>()
             .join(", ")
     }
@@ -105,8 +149,18 @@ impl Column {
 }
 
 fn get_tables(mut client: Client) -> Result<Vec<Table>, Box<dyn Error>> {
+    let table_sizes = client.query(REFLECT_TABLES_QUERY, &[])?.into_iter().fold(
+        HashMap::new(),
+        |mut acc: HashMap<String, i64>, row| {
+            let table_name: &str = row.get("table_name");
+            let table_size: i64 = row.get("table_size");
+
+            acc.insert(table_name.to_owned(), table_size);
+            acc
+        },
+    );
     let mut tables: Vec<Table> = client
-        .query(REFLECT_QUERY, &[])?
+        .query(REFLECT_COLUMNS_QUERY, &[])?
         .into_iter()
         .fold(
             HashMap::new(),
@@ -128,7 +182,12 @@ fn get_tables(mut client: Client) -> Result<Vec<Table>, Box<dyn Error>> {
         .into_iter()
         .map(|(name, mut columns)| {
             columns.sort_by(|a, b| a.position.cmp(&b.position));
-            let table = Table { name, columns };
+            let size = table_sizes.get(&name).cloned().unwrap_or(0);
+            let table = Table {
+                name,
+                columns,
+                size,
+            };
             table
         })
         .collect();
