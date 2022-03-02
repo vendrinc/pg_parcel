ddb604ab4229a540962f2d3be57764b012772a04
Jacob Elder <jacob@blissfully.com>
2022-03-02 14:35:10 -0500

    Add command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use clap::Parser;
 use indicatif::{ProgressBar, ProgressStyle};
 use postgres::{Client, NoTls};
 use std::collections::HashMap;
@@ -19,16 +20,24 @@ static REFLECT_QUERY: &str = r#"
     order by table_name, ordinal_position;
 "#;
 
-static PARTITION_COLUMN: &str = "OrganizationId";
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(short, long)]
+    #[clap(default_value_t = String::from("19653bc3-57f4-429e-902f-bc04b0fca4dc"))]
+    id: String,
 
-// TODO obviously this needs to be dynamic
-static PARTITION_VALUE: &str = "19653bc3-57f4-429e-902f-bc04b0fca4dc";
-
-fn main() {
-    try_main().unwrap();
+    #[clap(short, long)]
+    #[clap(default_value_t = String::from("OrganizationId"))]
+    column: String,
 }
 
-fn try_main() -> Result<(), Box<dyn Error>> {
+fn main() {
+    let args = Args::parse();
+    try_main(&args).unwrap();
+}
+
+fn try_main(args: &Args) -> Result<(), Box<dyn Error>> {
     let client = Client::connect(DATABASE_URL, NoTls)?;
     // The weird `COPY TO` behavior requires some acrobatics that we avoid by
     // just having a separate connection for it.
@@ -42,10 +51,7 @@ fn try_main() -> Result<(), Box<dyn Error>> {
     pb.enable_steady_tick(250);
 
     for table in tables.iter() {
-        let copy_statement = format!(
-            r#"COPY ({}) TO stdout;"#,
-            table.copy_out_query(PARTITION_VALUE)
-        );
+        let copy_statement = format!(r#"COPY ({}) TO stdout;"#, table.copy_out_query(args));
         pb.set_message(format!("{:>30}", table.name));
         let mut reader = copy_client.copy_out(&copy_statement)?;
         let mut buf = vec![];
@@ -69,7 +75,7 @@ struct Table {
 }
 
 impl Table {
-    fn copy_out_query(&self, partition_column_value: &str) -> String {
+    fn copy_out_query(&self, args: &Args) -> String {
         let query = match self.name.as_str() {
             "googleAuthPermissions" => {
                 format!(
@@ -77,30 +83,33 @@ impl Table {
                         select {} from "googleAuthPermissions"
                         join "googleAuthTokens" on "googleAuthTokenId" = "googleAuthTokens".id
                         join "directoryUsers" on "GoogleUserId" = "directoryUsers"."externalId"
-                        where "directoryUsers"."OrganizationId" = '{}'
+                        where "directoryUsers"."{}" = '{}'
                     "#,
                     &self.column_list_qualified(),
-                    PARTITION_VALUE,
+                    args.column,
+                    args.id,
                 )
             }
-            "dailyExchangeRates" => {
-                self.default_copy_out_query(partition_column_value) + " where True = False"
-            }
-            _ => self.default_copy_out_query(partition_column_value),
+            "dailyExchangeRates" => self.default_copy_out_query(args) + " where True = False",
+            _ => self.default_copy_out_query(args),
         };
         query
     }
 
-    fn default_copy_out_query(&self, partition_column_value: &str) -> String {
+    fn default_copy_out_query(&self, args: &Args) -> String {
         let mut query = format!(r#"select {} from "{}""#, &self.column_list(), &self.name);
         if let Some(org_scope) = self
             .columns
             .iter()
-            .find(|column| column.name == PARTITION_COLUMN)
+            .find(|column| column.name == args.column)
         {
-            let mut where_clause = format!(r#""{PARTITION_COLUMN}" = '{partition_column_value}'"#);
+            let mut where_clause =
+                format!(r#""{column}" = '{id}'"#, column = args.column, id = args.id);
             if org_scope.is_nullable {
-                where_clause = format!(r#"({where_clause} or "{PARTITION_COLUMN}" is null)"#)
+                where_clause = format!(
+                    r#"({where_clause} or "{column}" is null)"#,
+                    column = args.column
+                )
             }
             query = format!("{query} where {where_clause}");
         }
